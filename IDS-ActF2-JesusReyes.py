86f6cc7fa62364abd31a5ef7bfa9611f8934be00
UnFredy <jesus_enriquexp@hotmail.com>
2025-10-17 01:00:20 -0600

    Actualice el codigo principal con comentarios

--- IDS-ActF2-JesusReyes.py
+++ IDS-ActF2-JesusReyes.py
@@ -1,9 +1,12 @@
+#Clase principal (Padre), representa cualquier personaje.
 class Personaje:
+    #Constructor
     def __init__(self, nombre, vida, poder):
         self.nombre = nombre
         self.vida = vida
-        self.poder = poder
+        self.poder = poder  #Poder de ataque o curación (depende del personaje)
         
+    #Metodo para recibir daño de un ataque    
     def recibir_daño(self, daño):
         self.vida -= daño
         if self.vida <= 0:
@@ -12,22 +15,25 @@ class Personaje:
             print(f"{self.nombre} ahora tiene {self.vida} puntos de vida")
         
     def accion(self, objetivo):
-        pass
-    
+        pass    #Metodo que cada subclase redefine
+
+#Clase hija que representa una planta que puede atacar    
 class PlantaAtaque(Personaje):
     def atacar(self, zombi):
         print(f"{self.nombre} ha atacado a {zombi.nombre} y ha causado {self.poder} puntos de daño")
-        zombi.recibir_daño(self.poder)
+        zombi.recibir_daño(self.poder)  #Llama al método recibir_daño del zombi, aplicando el poder de la planta como daño
 
+#Clase hija que representa un zombie que puede atacar    
 class Zombi(Personaje):
     def atacar(self, planta):
         print(f"{self.nombre} ha atacado a {planta.nombre} y ha causado {self.poder} puntos de daño")
         planta.recibir_daño(self.poder)
-        
+
+#Clase hija que representa una planta curativa      
 class PlantaCurativa(Personaje):
     def accion(self, objetivo):
         print(f"{self.nombre} cura a {objetivo.nombre} restaurando {self.poder} puntos de vida")
-        objetivo.vida += self.poder
+        objetivo.vida += self.poder #Aumenta la vida del objetivo usando el valor de poder
         print(f"{objetivo.nombre} ahora tiene {objetivo.vida} puntos de vida")
     
 lanzaguizantes = PlantaAtaque("Lanzaguizantes", 100, 10)
@@ -40,6 +46,5 @@ lanzaguizantes.atacar(zombi)
 zombi.accion(lanzaguizantes)
 zombi.atacar(lanzaguizantes)
 zombi.atacar(girasol)
-
 
 girasol.accion(lanzaguizantes)
