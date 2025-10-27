bdda434ff54f91ab1c84b446e016823685ecf951
UnFredy <jesus_enriquexp@hotmail.com>
2025-10-26 19:17:18 -0600

    Agregué el metodo "accion" en lugar de usar "ataque"

--- IDS-ActF2-JesusReyes.py
+++ IDS-ActF2-JesusReyes.py
@@ -19,32 +19,32 @@ class Personaje:
 
 #Clase hija que representa una planta que puede atacar    
 class PlantaAtaque(Personaje):
-    def atacar(self, zombi):
+    def accion(self, zombi):
         print(f"{self.nombre} ha atacado a {zombi.nombre} y ha causado {self.poder} puntos de daño")
         zombi.recibir_daño(self.poder)  #Llama al método recibir_daño del zombi, aplicando el poder de la planta como daño
 
 #Clase hija que representa un zombie que puede atacar    
 class Zombi(Personaje):
-    def atacar(self, planta):
+    def accion(self, planta):
         print(f"{self.nombre} ha atacado a {planta.nombre} y ha causado {self.poder} puntos de daño")
         planta.recibir_daño(self.poder)
 
 #Clase hija que representa una planta curativa      
 class PlantaCurativa(Personaje):
     def accion(self, objetivo):
-        print(f"{self.nombre} cura a {objetivo.nombre} restaurando {self.poder} puntos de vida")
-        objetivo.vida += self.poder #Aumenta la vida del objetivo usando el valor de poder
-        print(f"{objetivo.nombre} ahora tiene {objetivo.vida} puntos de vida")
+        if objetivo.vida <= 0:
+            print(f"{self.nombre} cura a {objetivo.nombre} restaurando {self.poder} puntos de vida")
+            objetivo.vida += self.poder #Aumenta la vida del objetivo usando el valor de poder
+            print(f"{objetivo.nombre} ahora tiene {objetivo.vida} puntos de vida")
     
 lanzaguizantes = PlantaAtaque("Lanzaguizantes", 100, 10)
-zombi = Zombi("Zombi", 100, 10)
+zombi = Zombi("Zombi", 100, 100)
 girasol = PlantaCurativa("Girasol", 100, 5)
 
 lanzaguizantes.accion(zombi)    
-lanzaguizantes.atacar(zombi)
+lanzaguizantes.accion(zombi)
 
 zombi.accion(lanzaguizantes)
-zombi.atacar(lanzaguizantes)
-zombi.atacar(girasol)
+zombi.accion(girasol)
 
 girasol.accion(lanzaguizantes)
